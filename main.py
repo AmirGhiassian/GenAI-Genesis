13b4caba75103e57ae3f887ae361cf22a6d41b4c
Nuggethead2003 <amirrezaghiassian@icloud.com>
2024-03-30 17:50:21 -0400

    Added test for gcloud auth

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+def generate_text(project_id: str, location: str) -> str:
+
+    model = GenerativeModel("gemini-1.0-pro-vision")
+
+    response = model.generate_content(
+            [
+                Part.from_uri("gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"),
+                "What is shown in this picture?"
+            ]
+            )
+    print(response)
+    return response.text
+
+
+if __name__ == "__main__":
+    generate_text("genai-genesis", "us-central1")
